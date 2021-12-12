33ff5220734b73d10bd5bf3d0398c470cc1de175
azuma <azuma20011224@gmail.com>
2021-12-12 18:40:38 +0900

    creating customer orders

--- django_shopping/myshop/orders/views.py
+++ django_shopping/myshop/orders/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
 
-# Create your views here.
+def order_create(reqeust):
+    cart = Cart(reqeust)
+    if reqeust.method == 'POST':
+        form = OrderCreateForm(reqeust.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                        product=item['product'],
+                                        price=item['price'],
+                                        quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            return render(reqeust,
+                          'orders/order/created.html',
+                          {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(reqeust,
+                  'orders/order/create.html',
+                  {'cart': cart, 'form': form})
