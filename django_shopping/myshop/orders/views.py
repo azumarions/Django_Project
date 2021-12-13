5740100c543b148e24c6e7f7db7ab1f286d192f6
azuma <azuma20011224@gmail.com>
2021-12-13 14:53:51 +0900

    extending the administration site with custom views

--- django_shopping/myshop/orders/views.py
+++ django_shopping/myshop/orders/views.py
@@ -1,10 +1,13 @@
 from django.http import request
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_list_or_404
+from .models import Order
 
 def order_create(request):
     cart = Cart(request)
@@ -31,3 +34,10 @@ def order_create(request):
     return render(request,
                   'orders/order/create.html',
                   {'cart': cart, 'form': form})
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+                  'admin/orders/order/detail.html',
+                  {'order': order})
