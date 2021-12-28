2e771bbc07c2c2dab2fd2df544b1b044ee37960c
azuma <azuma20011224@gmail.com>
2021-12-28 18:58:59 +0900

    managing modules and their contents

--- django_learning/educa/courses/urls.py
+++ django_learning/educa/courses/urls.py
@@ -33,4 +33,8 @@ urlpatterns = [
      path('content/<int:id>/delete/',
          views.ContentDeleteView.as_view(),
          name='module_content_delete'),
+
+     path('module/<int:module_id>/',
+          views.ModuleContentListView.as_view(),
+          name='module_content_list'),
 ]
